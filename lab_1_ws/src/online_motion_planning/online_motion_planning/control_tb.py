bccddda031358e32970c92834aabe6813cc892db
Alex108306 <giang.nht108201@gmail.com>
2026-03-04 12:27:08 +0100

    Update new version fixing spinning around

--- lab_1_ws/src/online_motion_planning/online_motion_planning/control_tb.py
+++ lab_1_ws/src/online_motion_planning/online_motion_planning/control_tb.py
@@ -21,7 +21,7 @@ class LowLevelMotionControl(Node):
         self.goal_pose_sub = self.create_subscription(PoseStamped, '/goal_pose', self.recieve_goal_pose, 10)
         # self.goal_pose_service = self.create_service(PoseStamped, '/goto', self.goto_calling)
         self.vel_pub = self.create_publisher(Twist, '/turtlebot/cmd_vel', 10)
-        self.timer = self.create_timer(0.1, self.controller_func)
+        self.timer = self.create_timer(0.01, self.controller_func)
         self.robot_vel = Twist()
         self.robot_pose = PoseStamped()
         self.goal_pose = None
@@ -43,18 +43,18 @@ class LowLevelMotionControl(Node):
             pass
         else:
             
-            k_w = 0.5
-            k_v = 0.5
+            k_v = 0.2
             x_g, y_g = self.goal_pose.position.x, self.goal_pose.position.y
             x, y = self.robot_pose.pose.position.x, self.robot_pose.pose.position.y
             robot_orientation = self.robot_pose.pose.orientation
             theta = quaternion_to_yaw(robot_orientation)
             inc_x = x_g - x
             inc_y = y_g - y
-            # self.get_logger().info('Error: "%s" "%s"' % (inc_x, inc_y))
-            w = k_w * (atan2(inc_y, inc_x) - theta)
+            self.get_logger().info('Pose: "%s" "%s"' % (x, y))
+            self.get_logger().info('Goal Pose: "%s" "%s"' % (x_g, y_g))
+            w = (atan2(inc_y, inc_x) - theta)
             # self.get_logger().info('Angular: "%s"' % abs(w))
-            if abs(w) < 0.1:
+            if abs(w) < 0.01:
                 v = k_v * ((inc_x ** 2 + inc_y ** 2) ** 0.5)
             else: 
                 v = 0.0
